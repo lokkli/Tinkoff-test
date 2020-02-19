1a72e9e148d44abe88dadd12b355d3c3d565a4e8
OUT-Urmancheev-SD <urmancheev.s.d@sberbank.ru>
2020-02-19 11:52:38 +0300

    simple test

--- app/src/androidTest/java/com/moonpi/swiftnotes/SimpleTest.java
+++ app/src/androidTest/java/com/moonpi/swiftnotes/SimpleTest.java
@@ -1,6 +1,33 @@
 package com.moonpi.swiftnotes;
 
-public class SimpleTest {
+import android.support.test.rule.ActivityTestRule;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withHint;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.CoreMatchers.allOf;
+
+public class SimpleTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void onClick() throws Exception {
+        onView(withId(R.id.newNote)).perform(click());
+        onView(allOf(withId(R.id.titleEdit), withHint("Title"))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.bodyEdit), withHint("Note"))).check(matches(isDisplayed()));
+    }
 }
 
